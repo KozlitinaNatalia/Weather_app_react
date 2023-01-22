749f84822b7d24c46b6f6d9b9733e4d543b0c80d
Nataliia Kozlitina <kozlitinanatalia.e@gmail.com>
2023-01-22 15:28:01 +0300

    add search engine

---
+++ src/Weather.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
+      // icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+  function search() {
+    const apiKey = "3b009f8c7f78b32731d19b7ddd5b7a4d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              {/* <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              /> */}
+              <button type="submit" className="submit">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
